888816020c4727d76e5c1f281cfabf4a8d2193fc
Perera Dev <nashi.codes@gmail.com>
2023-06-30 11:39:04 -0300

    implementing DateTime and TimeSpan

--- FullCourse/Main.cs
+++ FullCourse/Main.cs
@@ -1,11 +1,17 @@
-﻿namespace FullCourse;
+﻿using FullCourse.date_Time;
+
+namespace FullCourse;
 
 internal class MainClass
 {
     private static void Main()
     {
-        //RandomNumber.Guess();
-        //Division.main();
-        string_builder.stringBuilder.main();
+        // RandomNumber.Guess();
+
+        // Division.main();
+
+        // string_builder.StringBuilder.Main();
+
+        ExDateTime.main();
     }
 }
---
+++ FullCourse/date_Time/Ex_DateTime.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace FullCourse.date_Time;
+
+public class ExDateTime
+{
+    public static void main()
+    {
+        var birthday = new DateTime(2002, 10, 22);
+        Console.WriteLine(birthday.ToString(CultureInfo.CurrentCulture));
+        Console.WriteLine(birthday.DayOfWeek);
+
+        var date = new DateTime(birthday.Year, birthday.Month, birthday.Day);
+        date = date.AddDays(10);
+        Console.WriteLine(date.ToString(CultureInfo.CurrentCulture));
+
+        var lunchTime = new TimeSpan(12, 30, 0);
+        Console.WriteLine("Lunch time: {0}", lunchTime.ToString());
+
+        lunchTime = lunchTime.Subtract(new TimeSpan(0, 15, 0));
+        Console.WriteLine("New lunch time: {0}", lunchTime.ToString());
+    }
+}
