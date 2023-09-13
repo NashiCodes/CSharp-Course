574bcc3df0e69bc90d44798a93755b752719cb20
Perera Dev <nashi.codes@gmail.com>
2023-09-13 00:04:52 -0300

    dictionary exemple

---
+++ FullCourse/Ex_Dictionary/superHeroes_Dictionary.cs
@@ -0,0 +1,33 @@
+﻿namespace FullCourse.Ex_Dictionary;
+
+public class superHeroes_Dictionary
+{
+    public static void main()
+    {
+        var superheroes = new Dictionary<string, string>
+        {
+            { "Clark Kent", "Superman" },
+            { "Bruce Wayne", "Batman" },
+            { "Barry West", "Flash" },
+            { "Peter Parker", "Spiderman" },
+            { "Steve Rogers", "Captain America" },
+            { "Tony Stark", "Iron Man" }
+        };
+
+        superheroes.Remove("Barry West");
+
+        Console.WriteLine("Count: {0}", superheroes.Count);
+
+        Console.WriteLine("Clark Kent: {0}", superheroes.ContainsKey("Clark Kent"));
+        Console.WriteLine("Barry West: {0}", superheroes.ContainsKey("Barry West"));
+
+        superheroes.TryGetValue("Clark Kent", out var test);
+        Console.WriteLine("Clark Kent: {0}", test);
+
+        foreach (var item in superheroes) Console.WriteLine("{0}: {1}", item.Key, item.Value);
+
+        superheroes.Clear();
+
+        Console.WriteLine("Count: {0}", superheroes.Count);
+    }
+}
